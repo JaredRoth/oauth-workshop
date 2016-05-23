22bb1d7911d801c2b11a7415cb43230c1aa3e89f
Jared Roth <jared.roth@gmail.com>
2016-05-23 15:19:23 -0600

    Status after class

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
+
+  get "/auth/twitter", as: :twitter_login
+  get "/auth/twitter/callback", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
 end
